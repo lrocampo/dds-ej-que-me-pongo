3638336b8028306e24e97b8b707b1bebb897f9c0
valentinarau <vraurobayo@frba.utn.edu.ar>
2023-04-20 23:32:25 -0300

    modify pom to run tests

--- src/test/java/org/quemepongo/Models/PrendaTest.java
+++ src/test/java/org/quemepongo/Models/PrendaTest.java
@@ -10,26 +10,26 @@ class PrendaTest {
 
   @Test
   // 1) Se puede crear una prenda indicando correctamente los campos requeridos
-  void sePuedeCrearUnaPrendaIndicandoSusCamposSiSonValidos() {
+  public void sePuedeCrearUnaPrendaIndicandoSusCamposSiSonValidos() {
     assertDoesNotThrow(() -> new Prenda(TipoPrenda.BOTAS, Material.CUERO, Color.VERDE, Color.AZUL));
   }
 
   @Test
   // 2) No debe permitirse crear una prenda si falta algun campo requerido
-  void noSePuedeCrearUnaPrendaSiFaltaAlgunCampoRequerido() {
+  public void noSePuedeCrearUnaPrendaSiFaltaAlgunCampoRequerido() {
     CampoRequeridoException campoRequeridoException = assertThrows(CampoRequeridoException.class, () -> new Prenda(TipoPrenda.BOTAS, Material.CUERO, null));
     assertEquals("El campo color principal es requerido", campoRequeridoException.getMessage());
   }
 
   @Test
   // 3) Se puede crear una prenda aunque no se indique color secundario
-  void sePuedeCrearUnaPrendaSinIndicarColorSecundario() {
+  public void sePuedeCrearUnaPrendaSinIndicarColorSecundario() {
     assertDoesNotThrow(() -> new Prenda(TipoPrenda.BOTAS, Material.CUERO, Color.VERDE));
   }
 
   @Test
   // 3) Se puede obtener la categoria de una prenda
-  void sePuedeIdentificarLaCategoriaDeUnaPrenda() {
+  public void sePuedeIdentificarLaCategoriaDeUnaPrenda() {
     Prenda prenda = new Prenda(TipoPrenda.BOTAS, Material.CUERO, Color.VERDE);
     assertEquals(Categoria.CALZADO, prenda.getCategoria());
   }
